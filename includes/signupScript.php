9961e9b7db1447b4476c8126a206bd9a8ac64283
dsouza <dhesouzanorbert36@gmail.com>
2023-02-07 19:36:44 +0300

    checking the password

--- includes/signupScript.php
+++ includes/signupScript.php
@@ -48,7 +48,41 @@ if (isset($_POST['signup-submit'])) {
             exit(); 
         }
         else if () {
-            mysqli_stmt_bind_param($stmt, "s")
+            mysqli_stmt_bind_param($stmt, "s", userName);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+            $resultCheck = mysqli_stmt_num_rows($stmt); // the rows returned must be 0 or 1
+
+            if ($resultCheck > 0) {
+                header("Location: ../signup.php?error=usertaken&Email=".$userEmail);
+                exit();  
+            }
+            else {
+                // insert the user into the database
+                $insert = "INSERT INTO users (userName, userEmail, userpwd) VALUES (?, ?, ?)";
+                $stmt = mysqli_stmt_init($conn);
+
+                if (!mysqli_stmt_prepare($stmt, $insert)) {
+                    header("Location: ../signup.php?error=sqlError");
+                    exit(); 
+                }
+                else {
+                    // hasing the password 
+                    $hasdpwd = password_hash($userPassword, PASSWORD_DEFAULT);
+
+                    mysqli_stmt_bind_param($stmt, "sss", $userName, $userEmail, $hasdpwd);
+                    mysqli_stmt_execute($stmt);
+                    header("Location: ../signup.php?signup=success");
+                    exit(); 
+                }
+            }
         }
     }
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+}
+
+else {
+    header("Location: ../signup.php");
+    exit();
 }
